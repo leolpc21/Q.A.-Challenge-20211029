5a58d2b90773bf26baa1fc773302cf9047a52309
Leo Costa <leolpc21@hotmail.com>
2023-01-19 21:33:15 -0300

    adicionando validações de login, subindo o pageObject, função no commands e atualização do Readme

---
+++ cypress/e2e/pageLogin.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+import login from "../pageObject/login";
+
+describe('Validar navegação do Login', () => {
+
+  it('Clicar no botão Login', () => {
+    cy.visit('/')
+
+    login.clicarBotaoLogin()
+
+    cy.url()
+      .should('be.equal', Cypress.config().baseUrl + 'auth/signin/candidates')
+
+  });
+
+  it('Validar login incorreto', () => {
+
+    var email = 'invalido@gmail.com'
+    var senha = 'invalido1234'
+
+    cy.intercept('GET', 'https://beta.coodesh.com/auth/signin/candidates').as('candidates')
+
+    cy.visit('/auth/signin/candidates')
+      .esperar('candidates', 200)
+
+    login.clicarBotaoOKBanner()
+
+    login.preencherCampoEmail(email)
+    login.preencherCampoSenha(senha)
+    login.clicarBotaoEntrar()
+    login.validarAlerta()
+
+  });
+})
---
+++ cypress/pageObject/login/elementos.js
@@ -0,0 +1,8 @@
+exports.elemento = {
+  buttonLogin: 'a[href="/auth/signin/candidates"]',
+  inputEmail: '#email',
+  inputSenha: '#password',
+  buttonEntrar: 'button[type=submit]',
+  buttonOkBanner: '.banner-content #onetrust-accept-btn-handler',
+  toast: '[class*="toastr"]'
+};
